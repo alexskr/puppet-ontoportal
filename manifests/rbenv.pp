0e916698fa281f5845d368203e906466f4fd8d47
Alex Skrenchuk <alexskr@stanford.edu>
2023-05-17 14:09:24 -0700

    add class variable for bundler version

--- manifests/rbenv.pp
+++ manifests/rbenv.pp
@@ -1,8 +1,8 @@
 class ontoportal::rbenv(
-  String  $ruby_version    = $title,
-  String  $bundler_version = '2.4',
-  Boolean $global          = true
-){
+  String $ruby_version    = $title,
+  String $bundler_version = '2.4',
+  Boolean $global         = true
+) {
   class { 'rbenv': }
   -> rbenv::plugin { ['rbenv/rbenv-vars', 'rbenv/ruby-build']: }
   -> rbenv::build { $ruby_version: global => $global }
