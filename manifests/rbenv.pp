3dad8cf74bb7b4455a88f5c54dcc5958ab0c90c5
Alex Skrenchuk <alexskr@stanford.edu>
2023-08-15 14:13:02 -0700

    set rubygems_version to 3.4.18

--- manifests/rbenv.pp
+++ manifests/rbenv.pp
@@ -1,6 +1,6 @@
 class ontoportal::rbenv(
   String $ruby_version     = $title,
-  String $rubygems_version = '3.3.26',
+  String $rubygems_version = '3.4.18',
   String $bundler_version  = '2.4',
   Boolean $global          = true
 ) {
@@ -8,8 +8,8 @@ class ontoportal::rbenv(
   -> rbenv::plugin { ['rbenv/ruby-build']: latest => true }
   -> rbenv::build { $ruby_version:
 #    rubygems_version => $rubygems_version,
-    bundler_version  => $bundler_version,
-    global           => $global,
+    bundler_version => $bundler_version,
+    global          => $global,
   }
   # for unknown reason setting rubygems_version in the rbenv::build fails so we have a work around:
   -> exec { "upgrade rubygems to version ${rubygems_version}":
