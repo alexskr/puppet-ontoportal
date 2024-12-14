1f6014efe4cc213d407198b93681724a803ec77d
Alex Skrenchuk <alexskr@stanford.edu>
2024-12-13 16:43:10 -0800

    fix build issues on ubuntu

--- manifests/rbenv.pp
+++ manifests/rbenv.pp
@@ -1,11 +1,17 @@
 define ontoportal::rbenv(
   String $ruby_version     = $title,
-  String $rubygems_version = '3.4.22',
-  String $bundler_version  = '2.4.22',
+  String $rubygems_version = '3.5.20',
+  String $bundler_version  = '2.5.20',
   Boolean $global          = true
 ) {
+
+  #ruby can take a while to compile
   Exec { timeout => 1800 }
-  include rbenv
+
+  class{ 'rbenv':
+    env => ["TMPDIR=/var/tmp"], #ubuntu mounts /tmp with noexec by defualt which breaks ruby build
+  }
+
   if !defined(Rbenv::Plugin['rbenv/ruby-build']) {
     rbenv::plugin { ['rbenv/ruby-build']: latest => true }
   }
@@ -14,8 +20,4 @@ define ontoportal::rbenv(
     bundler_version  => $bundler_version,
     global           => $global,
   }
-  # for unknown reason setting rubygems_version in the rbenv::build fails so we have a work around:
-  #  -> exec { "upgrade rubygems to version ${rubygems_version}":
-  #  command => "/usr/local/rbenv/shims/gem update --system ${rubygems_version}",
-  #  unless  => "/usr/local/rbenv/shims/gem --version | grep -q ${rubygems_version}",
 }
