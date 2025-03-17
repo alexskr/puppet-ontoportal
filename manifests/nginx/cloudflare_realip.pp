cfd9b641e4a0e6f9dc96d25c663dee79fd50895d
Alex Skrenchuk <alexskr@stanford.edu>
2025-03-17 15:04:33 -0700

    precrete nginx config file, so that it doesn't get purged

--- manifests/nginx/cloudflare_realip.pp
+++ manifests/nginx/cloudflare_realip.pp
@@ -5,18 +5,27 @@ class ontoportal::nginx::cloudflare_realip (
   # realip nginx extention comes in nginx-extras
   ensure_packages(['nginx-extras'])
 
-  file { '/usr/local/bin/update_cloudflare_ips.sh':
+  file { '/etc/nginx/conf.d/cloudflare_real_ip.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => 'puppet:///modules/ontoportal/nginx-cloudflare_real_ip.conf',
+    replace =>  false, #rely on the cronjob to get latest updates"
+  }
+
+  file { '/usr/local/bin/nginx-cloudflare_real_ip.sh':
       ensure => 'present',
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
-      source => 'puppet:///modules/ontoportal/nginx_cloudflare_realip.sh'
+      source => 'puppet:///modules/ontoportal/nginx-cloudflare_real_ip-cron.sh'
   }
 
   # Add weekly cron job to update Cloudflare IPs
   cron { 'update_cloudflare_real_ip':
     ensure  => $ensure,
-    command => "/usr/local/bin/update_cloudflare_ips.sh",
+    command => "/usr/local/bin/nginx-cloudflare_real_ip.sh",
     user    => 'root',
     weekday => '1',  # Runs every Monday
     hour    => '0',
