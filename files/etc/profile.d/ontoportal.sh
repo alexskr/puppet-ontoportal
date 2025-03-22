9464fadd822ca7fbc1575a953818cc75d12fe777
Alex Skrenchuk <alexskr@stanford.edu>
2025-03-21 22:13:00 -0700

    add useful aliases

---
+++ files/etc/profile.d/ontoportal.sh
@@ -0,0 +1,9 @@
+#useful aliases for OntoPortal
+alias suo="sudo su - ontoportal"
+alias gtc="pushd /opt/ontoportal/ncbo_cron"
+alias gta="pushd /opt/ontoportal/ontologies_api/current"
+alias gtu="pushd /opt/ontoportal/bioportal_web_ui/current"
+alias gtv="pushd /opt/ontoportal/virtual_appliance/"
+alias tla="tail -f /opt/ontoportal/ontologies_api/current/log/appliance.log"
+alias tlu="tail -f /opt/ontoportal/bioportal_web_ui/current/log/appliance.log"
+alias tlc="tail -f /opt/ontoportal/ncbo_cron/logs/scheduler.log"
