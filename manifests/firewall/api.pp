d097f68be9ca4056fbe1f337e2cda2f953a613fe
Alex Skrenchuk <alexskr@stanford.edu>
2025-03-25 15:29:34 -0700

    dependency tweaks

--- manifests/firewall/api.pp
+++ manifests/firewall/api.pp
@@ -1,4 +1,5 @@
 class ontoportal::firewall::api {
+  include ontoportal::firewall
   firewall_multi { '110 Allow inbound rest api':
     dport => ['8080','8443'],
     proto => 'tcp',
--- manifests/firewall/ssh.pp
+++ manifests/firewall/ssh.pp
@@ -1,4 +1,5 @@
 class ontoportal::firewall::ssh {
+  include ontoportal::firewall
   firewall_multi { '020 Allow inbound SSH':
     dport => '22',
     proto => 'tcp',
