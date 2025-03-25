41b3ef144f95186f5e8cb23e3e90f18373914626
Alex Skrenchuk <alexskr@stanford.edu>
2025-03-24 23:51:17 -0700

    make sure ssl-cert is installed

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,38 +1,25 @@
 #
-# Class: ncbo::ontologies_api
+# Class: ontoportal::nginx
 #
-# Description: This class manages common thing for ncbo bioportal core
-# nginx - unicorn https://github.com/defunkt/unicorn/blob/master/examples/nginx.conf
-# Parameters:
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
+# Description: wrapper class for managing common things for nginx
 #
 class ontoportal::nginx (
-
-  Enum['staging', 'production', 'appliance', 'development'] $environment = 'staging',
   Integer $logrotate_nginx     = 180,
-  Stdlib::Host $domain         = 'data.demo.ontoportal.org',
-  Boolean $enable_letsencrypt  = false,
-  Stdlib::Port $port           = 80,
-  Stdlib::Port $ssl_port       = 443,
   Boolean $enable_nginx_status = false,
   Boolean $manage_nginx_repo   = true,
   Boolean $manage_firewall     = true,
   Boolean $manage_logrotate    = true,
   Integer $logrotate_days      = 180,
-) inherits ontoportal::params {
-
+) {
   if $manage_firewall {
     firewall_multi { "34 allow inbound on port ${port}":
-      dport  => [$port, $ssl_port],
-      proto  => tcp,
-      jump => accept,
+      dport => [$port, $ssl_port],
+      proto => tcp,
+      jump  => accept,
     }
   }
+
+  ensure_packages(['ssl-cert']) # needed for snakeoil ssl cert
 
   class { 'nginx':
     manage_repo              => $manage_nginx_repo,
@@ -43,11 +30,11 @@ class ontoportal::nginx (
     gzip_types               => 'text/plain text/css application/x-javascript text/xml application/xml application/xml+rss application/json text/javascript',
     server_purge             => true,
     confd_purge              => true,
+    require                  => Package['ssl-cert'],
   }
 
   # Global Nginx Status Page
   if $enable_nginx_status {
-
     nginx::resource::server { 'localhost':
       ensure      => present,
       listen_port => 80,
@@ -69,6 +56,4 @@ class ontoportal::nginx (
       logrotate_days => $logrotate_days,
     }
   }
-
-
 }
