fd9da782ec9504650210e9a64232daad46263d54
Alex Skrenchuk <alexskr@stanford.edu>
2025-04-24 10:44:18 -0700

    add manage password option for sysadmin user

--- manifests/user/sysadmin.pp
+++ manifests/user/sysadmin.pp
@@ -1,15 +1,23 @@
 class ontoportal::user::sysadmin (
-  String              $user      = 'ubuntu',
-  Array[String]       $sshkeys = [],
-  String              $shell     = '/bin/bash',
-  String              $comment   = 'System SSH user with sudo access',
-  Boolean             $managehome = true,
+  String           $user       = 'ubuntu',
+  Array[String]    $sshkeys    = [],
+  String           $shell      = '/bin/bash',
+  String           $comment    = 'OntoPortal SysAdmin user',
+  Optional[String] $password   = undef,
+  Boolean          $managehome = true,
 ) {
+  # don't set password for ubuntu user in ami
+  $_password = $facts['ec2_metadata'] ? {
+    undef   => $password, # not on AWS
+    default => undef,     # on AWS, disable password
+  }
+
   accounts::user { $user:
     ensure     => present,
     comment    => $comment,
     shell      => $shell,
     home       => "/home/${user}",
+    password   => $_password,
     managehome => $managehome,
     sshkeys    => $sshkeys,
   }
