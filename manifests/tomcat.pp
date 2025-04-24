dad9cf5d89da409d18ff5ffca2113942d4e93c9f
Alex Skrenchuk <alexskr@stanford.edu>
2025-04-24 11:10:19 -0700

    update tomcat version

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -2,7 +2,7 @@
 # a wrapper class for pupppetlabs/tomcat module.
 # installs tomcat9 from source
 class ontoportal::tomcat(
-  String $version                     = '9.0.102',
+  String $version                     = '9.0.104',
   Stdlib::Port $port                  = 8080,
   Stdlib::HTTPUrl $source_url = "https://archive.apache.org/dist/tomcat/tomcat-9/v${version}/bin/apache-tomcat-${version}.tar.gz",
   Boolean $webadmin                   = false,
